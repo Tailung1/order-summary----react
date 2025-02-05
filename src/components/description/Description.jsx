62f56fd139b97797369b87b3119cbe3d2f26a0d1
Tailung1 <ramazsamitashvili11@gmail.com>
2025-02-05 20:46:05 +0400

    write full html structure

--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -8,11 +8,19 @@ export default function Description() {
             <h1>Order Summary</h1>
             <p>You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!</p>
 
-            <div>
-                <section className="annual-div">
+            <div className="annual-div">
+                <section className="plan-div">
                 <img src={musicLogo} alt="music-icon" />
+
+                <section className="plan">
+                    <span>Annual Plan</span>
+                    <span>$59.99/year</span>
                 </section>
+                </section>
+                <p>Change</p>
             </div>
+            <button>Proceed to Payment</button>
+            <button>Cancel Order</button>
         </div>
         </>
     )
